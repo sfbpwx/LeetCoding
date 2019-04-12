30e2241365264b86640d6aa4d79f6e62d53d9089
sfbpwx <124008127@users.noreply.github.com>
2019-04-12 20:18:58 +0800

    回文数的问题

---
+++ src/easy/weekOne/Code_9_IsPalindrome.java
@@ -0,0 +1,29 @@
+package easy.weekOne;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Code_9_IsPalindrome {
+	public boolean isPalindrome(int x) {
+		if(x>Integer.MAX_VALUE||x<Integer.MIN_VALUE){
+			return false;
+		}
+        Long xs = Math.abs(Long.valueOf(x));
+        xs = (x>=0?xs:-xs);
+        List<Long> list = new ArrayList<>();
+        String resultLong = "";
+        do{
+        	list.add(xs%10);
+        }while((xs/=10)>0);
+        for(int i=0;i<list.size();i++){
+        	resultLong+=list.get(i);
+        }
+        resultLong = (x>=0?resultLong:resultLong+"-");
+        return (String.valueOf(x).equals(resultLong)?true:false);
+    }
+	
+	public static void main(String args[]){
+		Code_9_IsPalindrome a = new Code_9_IsPalindrome();
+		System.out.println(a.isPalindrome(0));
+	}
+}
