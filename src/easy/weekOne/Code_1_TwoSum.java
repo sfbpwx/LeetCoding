74706a69bf54a8c85cc4f86f5ee48c0f6ecc406a
sfbpwx <124008127@users.noreply.github.com>
2019-04-12 16:50:31 +0800

    FIRST

---
+++ src/easy/weekOne/Code_1_TwoSum.java
@@ -0,0 +1,42 @@
+package easy.weekOne;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Code_1_TwoSum {
+	public int[] twoSum_solution1(int[] nums, int target) {
+        int[] result = null;
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            for(Integer key : map.keySet()){ 
+                if(target==nums[i]+key){
+                    result = new int[]{map.get(key),i};
+                    System.out.println(result);
+                }
+            }
+            map.put(nums[i],i);   
+        }
+        return result;
+    }
+	public int[] twoSum_solution2(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(map.get(target-nums[i])!=null){
+                return new int[]{map.get(target-nums[i]),i};
+            }
+            map.put(nums[i],i);   
+        }
+//        Iterator<Map.Entry<Integer,Integer>> mapA = map.entrySet().iterator();
+//        while(mapA.hasNext()){
+//        	Map.Entry<Integer, Integer> entry = mapA.next(); 
+//        	entry.getKey();
+//        	entry.getValue();
+//        }
+        return null;
+        
+        
+       
+    }
+}
+
