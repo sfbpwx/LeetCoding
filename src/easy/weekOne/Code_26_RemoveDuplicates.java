e96d89cad727a64991d7e437c04c664d432b0b5a
sfbpwx <124008127@qq.com>
2019-04-14 02:10:34 +0800

    快慢指针，增加相应备注

--- src/easy/weekOne/Code_26_RemoveDuplicates.java
+++ src/easy/weekOne/Code_26_RemoveDuplicates.java
@@ -16,9 +16,15 @@ public class Code_26_RemoveDuplicates {
 		int left = 0; // slow point
 		for (int i = 0; i < nums.length; i++) { // fast point
 			if (nums[left] != nums[i]) {
-				nums[++left] = nums[i];
+				nums[++left] = nums[i];//将需要的数复制到慢指针左边，形成可用的数组
 			}
 		}
 		return left + 1;
+	}
+	
+	public static void main(String args[]){
+		Code_26_RemoveDuplicates a = new Code_26_RemoveDuplicates();
+		int[] nums = new int[]{1,1,2,2,3,4,5,6};
+		a.removeDuplicates(nums);
 	}
 }
