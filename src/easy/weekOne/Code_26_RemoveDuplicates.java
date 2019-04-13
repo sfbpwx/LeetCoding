72ad9b50b3bc5ea14ea7a00cc6379357a5eb939f
sfbpwx <124008127@qq.com>
2019-04-14 01:39:21 +0800

    快慢指针方法 具体看描述

--- src/easy/weekOne/Code_26_RemoveDuplicates.java
+++ src/easy/weekOne/Code_26_RemoveDuplicates.java
@@ -6,6 +6,7 @@ public class Code_26_RemoveDuplicates {
 	 * @param nums
 	 * 该题与 27. 移除元素 类似，都运用了快慢双指针来解决。
 	 * 同样，我们可以用慢指针来指向下一个不重复元素的插入位置，快指针用于迭代，检测慢指针指向的元素是否和当前遍历到的元素重复。如果重复则插入到left所指的位置即可：
+	 * 最后，慢指针的值 + 1 即元素剩余不重复元素的值。
 	 * @return
 	 */
 	public int removeDuplicates(int[] nums) {
