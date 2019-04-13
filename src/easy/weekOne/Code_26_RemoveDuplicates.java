798b1a1b382dc9dd4b717ea0422d9a293cdcc7cc
sfbpwx <124008127@qq.com>
2019-04-14 01:38:33 +0800

    快慢指针 原题：删除数组重复项 还需研究

---
+++ src/easy/weekOne/Code_26_RemoveDuplicates.java
@@ -0,0 +1,23 @@
+package easy.weekOne;
+
+public class Code_26_RemoveDuplicates {
+	/**
+	 * 
+	 * @param nums
+	 * 该题与 27. 移除元素 类似，都运用了快慢双指针来解决。
+	 * 同样，我们可以用慢指针来指向下一个不重复元素的插入位置，快指针用于迭代，检测慢指针指向的元素是否和当前遍历到的元素重复。如果重复则插入到left所指的位置即可：
+	 * @return
+	 */
+	public int removeDuplicates(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return 0;
+
+		int left = 0; // slow point
+		for (int i = 0; i < nums.length; i++) { // fast point
+			if (nums[left] != nums[i]) {
+				nums[++left] = nums[i];
+			}
+		}
+		return left + 1;
+	}
+}
