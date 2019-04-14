2bb90b054703a8d74e97324cd5b5737b3ea825a8
sfbpwx <124008127@users.noreply.github.com>
2019-04-14 22:01:36 +0800

    找到数组的插入点

---
+++ src/easy/weekOne/Code_35_SearchInsert.java
@@ -0,0 +1,11 @@
+package easy.weekOne;
+
+public class Code_35_SearchInsert {
+	public int searchInsert(int[] nums, int target) {
+		int first = 0;
+        int second = 0;
+        int[] result = new int[nums.length+1];
+//        result.
+        return 0;
+    }
+}
