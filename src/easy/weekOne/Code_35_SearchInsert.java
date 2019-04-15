d058aad500fe2416bc2da0cdff50b7cf0d2cc0c0
sfbpwx <124008127@users.noreply.github.com>
2019-04-15 09:38:34 +0800

    一堆if判断解决问题

--- src/easy/weekOne/Code_35_SearchInsert.java
+++ src/easy/weekOne/Code_35_SearchInsert.java
@@ -2,10 +2,13 @@ package easy.weekOne;
 
 public class Code_35_SearchInsert {
 	public int searchInsert(int[] nums, int target) {
-		int first = 0;
-        int second = 0;
-        int[] result = new int[nums.length+1];
-//        result.
-        return 0;
+		if(nums.length==0)return 0;
+		if(nums[0]>target)return 0;
+		if(nums[nums.length-1]<target)return nums.length;
+		for(int i=0;i<nums.length;i++){
+            if(nums[i]==target) return i;
+			if(nums[i]<target&&nums[i+1]>target)return i+1;
+		}
+		return 0;
     }
 }
