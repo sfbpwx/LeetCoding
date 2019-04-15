3b6c05be52168ddcfdb17ba8f0849b05cccb532d
sfbpwx <124008127@users.noreply.github.com>
2019-04-15 09:48:55 +0800

    增加折半查找方法

--- src/easy/weekOne/Code_35_SearchInsert.java
+++ src/easy/weekOne/Code_35_SearchInsert.java
@@ -11,4 +11,28 @@ public class Code_35_SearchInsert {
 		}
 		return 0;
     }
+	/**
+	 * 二分查找
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int searchInsert2(int[] nums, int target) {
+        // 二分查找
+        if(nums.length == 0)
+            return -1;
+        int left = 0;
+        int right = nums.length-1;
+        while (left <= right){
+            int mid = (left+right)/2;
+            if(target == nums[(left+right)/2])
+                return (left+right)/2;
+            if(target<nums[mid]){
+                right = mid-1;
+            }else {
+                left = mid+1;
+            }
+        }
+        return left;
+    }
 }
