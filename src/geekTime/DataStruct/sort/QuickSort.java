d8e36055bafa673e50df06ae6eeb2947b127a66c
124008127@qq.com <Wangxian1989>
2020-01-13 09:55:45 +0800

    快速排序的注释

--- src/geekTime/DataStruct/sort/QuickSort.java
+++ src/geekTime/DataStruct/sort/QuickSort.java
@@ -2,17 +2,24 @@ package geekTime.DataStruct.sort;
 
 public class QuickSort {
     public static int partition(int[] unsorted, int low, int high){
+        //默认中位数为中指针
+        int[] old  = unsorted;
         int pivot = unsorted[low];
         while (low < high){
+            //当高指针大于中位数时，高指针递减，
             while (low < high && unsorted[high] > pivot) {
                 high--;
             }
+            //将低指针的值覆盖为高指针的值
             unsorted[low] = unsorted[high];
+            //当低指针小于中位数时，低指针递增
             while (low < high && unsorted[low] <= pivot) {
                 low++;
             }
+            //将高指针的值覆盖为低指针
             unsorted[high] = unsorted[low];
         }
+        //将暂存的低指针原有值重新赋给低指针
         unsorted[low] = pivot;
         return low;
     }
@@ -31,7 +38,7 @@ public class QuickSort {
 
     public static void main(String[] args)
     {
-        int[] x = { 6, 2, 4, 1, 5, 9, 23,51,42,15};
+        int[] x = { 6, 2, 8, 1, 5, 9, 23,51,42,15};
         quick_sort(x, 0, x.length - 1);
         for(int item:x){
             System.out.print(item+",");
